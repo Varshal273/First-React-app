9022233348ed5ad013c84ae04dd5f8fe01d76609
Varshal <jadvanivarshal237@gmail.com>
2024-08-10 15:32:18 +0530

    state in react | added summary and preview  in TextForm and Lower Case button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
-      <Navbar title='TextUtils' aboutText='about TextUtils' />
+      <Navbar title='TextUtils' />
       {/* <Navbar/> */}
+      <div className="container my-3">
+        <TextForm heading='Enter the Text to Analyze Below'/>
+      </div>
     </>
   );
 }
